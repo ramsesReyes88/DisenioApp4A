71adbd64435eaaa49af720de699a242f74d74602
ramsesReyes88 <150385789+ramsesReyes88@users.noreply.github.com>
2024-03-15 08:44:37 -0700

    Add files via upload

--- Portafolio/SegundoPortafolio/pokeapi/App.js
+++ Portafolio/SegundoPortafolio/pokeapi/App.js
@@ -1,1 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
+import PokeStart from "./components/PokeStart.js";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <PokeStart/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
